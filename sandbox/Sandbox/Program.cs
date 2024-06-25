3d5aa719c17cf1ae237d1cd720f2ada395552b79
John Asada <johnasada6@gmail.com>
2024-06-25 02:50:19 -0600

    code eddited

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello sandbox World!!!!");
+        Console.WriteLine("Hello sandbox World!!!");
+        Console.WriteLine("This is c#");
+
+        Console.Write("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your color is {color}");
     }
 }
